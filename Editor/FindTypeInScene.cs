4d4507a78dc49a67cfc03fafc07764a7164eaf34
baba-s <shotababa1206@gmail.com>
2022-08-30 20:37:44 +0900

    update

--- Editor/FindTypeInScene.cs
+++ Editor/FindTypeInScene.cs
@@ -1,7 +1,4 @@
-﻿using System.Linq;
-using System.Reflection;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEditor;
 
 namespace Kogane.Internal
 {
@@ -11,32 +8,13 @@ namespace Kogane.Internal
         private static void Find( MenuCommand menuCommand )
         {
             var type = menuCommand.context.GetType();
-            SetSearchFilter( $"t:{type.Name}" );
-        }
 
-        private enum FilterMode
-        {
-            ALL,
-            NAME,
-            TYPE,
-        }
-
-        private static void SetSearchFilter( string filter, FilterMode filterMode = FilterMode.ALL )
-        {
-            var hierarchy = Resources
-                    .FindObjectsOfTypeAll<SearchableEditorWindow>()
-                    .FirstOrDefault( x => x.GetType().ToString() == "UnityEditor.SceneHierarchyWindow" )
-                ;
-
-            if ( hierarchy == null ) return;
-
-            var setSearchType = typeof( SearchableEditorWindow )
-                    .GetMethod( "SetSearchFilter", BindingFlags.NonPublic | BindingFlags.Instance )
-                ;
-
-            var parameters = new object[] { filter, ( int )filterMode, false, false };
-
-            setSearchType.Invoke( hierarchy, parameters );
+            SceneHierarchyInternal.SetSearchFilter
+            (
+                searchFilter: $"t:{type.Name}",
+                searchMode: SearchableEditorWindow.SearchMode.All,
+                setAll: true
+            );
         }
     }
 }
